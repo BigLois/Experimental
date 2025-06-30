e42be535df4a4b951c25af8c3f041eb6575a5637
BigLois <lois.bigot@epitech.eu>
2025-06-30 16:41:36 +0400

    add game button

--- js/script.js
+++ js/script.js
@@ -9,3 +9,15 @@ function tirerCarte() {
   carteImage.src = imagesCartes[index];
   carteImage.style.display = "block";
 }
+
+const imagesCartes2 = [
+  "../src/c1.jpg",
+  "../src/c2.jpg",
+];
+
+function tirerCarte2() {
+  const index2 = Math.floor(Math.random() * imagesCartes2.length);
+  const carteImage2 = document.getElementById("carte2");
+  carteImage2.src = imagesCartes2[index2];
+  carteImage2.style.display = "block";
+}
